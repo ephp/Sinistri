ea389361ebd96fb2563c5783c104f961e090dca2
Ephraim <ephraim.pepe@gmail.com>
2013-08-13 16:34:25 +0200

    fix2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,7 +39,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new B2S\Bundle\GeneratorBundle\B2SGeneratorBundle();
         }
 
         return $bundles;
